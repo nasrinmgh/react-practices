9e08e85f048348cfbee98a6e4cf9c4965149de2a
nasrinmgh <nasrinmohaghegh98@gmail.com>
2025-10-05 17:36:07 +0200

    initialize recipe app

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ProfileCard from "./components/ProfileCard";
-import Button from "./components/Button";
 
-function App() {
-  return (
-    <>
-      <>
-        <ProfileCard
-          image={"https://picsum.photos/200/300"}
-          title={"John Doe"}
-          description={"Short description about card"}
-        />
-      </>
-      <>
-        <ProfileCard
-          image={"https://picsum.photos/200/300"}
-          title={"Card 2"}
-          description={"Another bullshit description,"}
-        />
-      </>
-      <>
-        <ProfileCard
-          image={"https://picsum.photos/200/300"}
-          title={"Card 3"}
-          description={"Well another description"}
-        />
-      </>
-    </>
-  );
-}
+function App() {}
 
 export default App;
--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ label, onClick, variant }) {
-  return (
-    <button className={`btn btn-${variant}`} onClick={onClick}>
-      {label}
-    </button>
-  );
-}
-
-export default Button;
